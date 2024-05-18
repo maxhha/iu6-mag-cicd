5ae722bccb6c7bc4b19e716e0f99b38f8e4aaf97
devmaxhha <devmaxhha@gmail.com>
2024-05-18 21:08:58 +0300

    Add init container

--- docker-compose-template/backend/init_db.py
+++ docker-compose-template/backend/init_db.py
@@ -1,3 +1,15 @@
-from base import Base, engine
+from contextlib import closing
+from base import Base, SessionLocal, User, engine
 
 Base.metadata.create_all(bind=engine)
+
+users = [
+     User(login='pavel', email='a@gmail.com'),
+     User(login='yura', email='b@gmail.com')
+]
+
+with closing(SessionLocal()) as session:
+    for user in users:
+        if session.query(User.id).filter_by(email=user.email).first():
+            session.add(user)
+            session.commit()
