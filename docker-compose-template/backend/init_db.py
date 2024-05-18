fb560eddb6a265682cf7007e1f0b09f28ea8b458
devmaxhha <devmaxhha@gmail.com>
2024-05-18 22:03:10 +0300

    Add frontend service

--- docker-compose-template/backend/init_db.py
+++ docker-compose-template/backend/init_db.py
@@ -1,6 +1,7 @@
 from contextlib import closing
 from base import Base, SessionLocal, User, engine
 
+print("Create all tables")
 Base.metadata.create_all(bind=engine)
 
 users = [
@@ -8,8 +9,12 @@ users = [
      User(login='yura', email='b@gmail.com')
 ]
 
+print("Create users:")
 with closing(SessionLocal()) as session:
     for user in users:
         if session.query(User.id).filter_by(email=user.email).first():
+            print(f"- {user.email} exists")
+        else:
             session.add(user)
             session.commit()
+            print(f"- {user.email} added")
